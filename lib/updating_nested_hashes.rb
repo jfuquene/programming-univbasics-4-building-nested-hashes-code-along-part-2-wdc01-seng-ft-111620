9267d9257a2d2b01c627a36e0b2aed79db195873
John Fuquene <jfuquene@email.wm.edu>
2020-11-08 00:25:51 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,9 +51,9 @@ def add_characters
    }
 
 
-    epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
+    epic_tragedy[:additional_characters] << {:name "Prince Escalus"}
     
-    epic_tragedy[:additional_characters] << {name: "Apothecary"}
+    epic_tragedy[:additional_characters] << {:name "Apothecary"}
     
    
    epic_tragedy
